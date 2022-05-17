f796e8c1c225ecaa6a23d8681bf8c42ca09e4269
Vlad Brok <vlad.brok99@gmail.com>
2022-05-17 14:15:41 +0300

    Add more endpoints

--- Server/EmploymentAgency/Program.cs
+++ Server/EmploymentAgency/Program.cs
@@ -1,8 +1,11 @@
+using System;
+using System.CodeDom.Compiler;
+using System.Linq;
+using System.Threading.Tasks;
 using EmploymentAgency.Models;
 
 /**
     * update или delete нарушает ограничение внешнего клуча
-    * Read
     * Reports
     * Timeouts and error handling
     
@@ -55,45 +58,56 @@ PostgreSql MakePostgres()
 
 void MapCrudQueries()
 {
-    foreach (string table in new[]
-    {
-        "addresses",
-        "applications",
-        "districts",
-        "employers",
-        "employment_types",
-        "positions",
-        "properties",
-        "seekers",
-        "statuses",
-        "streets",
-        "vacancies",
-    })
-    {
-        string endpoint = $"api/{table}";
+    var mainTables = new (string Name, ReadAsyncFunc ReadAsync)[] {
+        ("addresses", ReadAddressesAsync),
+        ("applications", ReadApplicationsAsync),
+        ("employers", ReadEmployersAsync),
+        ("seekers", ReadSeekersAsync),
+        ("streets", ReadStreetsAsync),
+        ("vacancies", ReadVacanciesAsync),
+    };
+    var referenceTableNames =
+        new[] { "change_log", "districts", "employment_types", "positions", "properties", "statuses" };
 
-        app.MapPost(endpoint, async (Entity entity) =>
+    foreach (var table in mainTables
+                            .Concat(referenceTableNames
+                                .Select<string, (string Name, ReadAsyncFunc ReadAsync)>(n => (
+                                    n,
+                                    (int page, int pageSize, int? id) => ReadAsync(page, pageSize, n, id)))))
+    {
+        app.MapGet(
+            getEndpoint(table.Name),
+            async (int page, int pageSize) => await table.ReadAsync(page, pageSize));
+
+        app.MapGet(
+            getEndpointWithId(table.Name),
+            async (int id) => await table.ReadAsync(0, 1, id));
+
+        app.MapPost(getEndpoint(table.Name), async (Entity entity) =>
         {
             await postgres.ExecuteAsync(
-                $@"INSERT INTO {table} ({string.Join(", ", entity.Keys)}) 
+                $@"INSERT INTO {table.Name} ({string.Join(", ", entity.Keys)}) 
                VALUES ('{string.Join("', '", entity.Values)}')");
         });
 
-        app.MapPut($"{endpoint}/{{id}}", async (int id, Entity entity) =>
+        app.MapPut(getEndpointWithId(table.Name), async (int id, Entity entity) =>
         {
             await postgres.ExecuteAsync(
-                $@"UPDATE {table} 
+                $@"UPDATE {table.Name} 
                SET {string.Join(", ", entity.Select(e => $"{e.Key} = '{e.Value}'"))}
                WHERE id = {id}");
         });
 
-        app.MapDelete($"{endpoint}/{{id}}", async (int id) =>
+        app.MapDelete(getEndpointWithId(table.Name), async (int id) =>
         {
             await postgres.ExecuteAsync(
-                $@"DELETE FROM {table} 
+                $@"DELETE FROM {table.Name} 
                WHERE id = {id}");
         });
     }
+
+    string getEndpoint(string table) => $"api/{table}";
+    string getEndpointWithId(string table) => $"{getEndpoint(table)}/{{id}}";
 }
 
 void MapSpecialQueries(string root)
@@ -181,10 +195,86 @@ void MapSpecialQueries(string root)
             await ReadAsync(page, pageSize, $"get_latest_vacancy_of_employers_whose_name_contains('{{pattern}}')"));
 }
 
-async Task<IResult> ReadAsync(int page, int pageSize, string target)
+
+async Task<IResult> ReadEmployersAsync(int page, int pageSize, int? id = null)
 {
-    return Results.Ok(await postgres.ExecuteAsync(
-        $@"SELECT * FROM {target} 
-           OFFSET {page * pageSize}
-           LIMIT {pageSize};"));
+    return await ReadPageAsync(page, pageSize,
+        $@"SELECT e.id, e.employer, p.property, 
+                  e.phone, e.email, d.district, 
+                  s.street, a.building_number, s.postal_code
+           FROM employers e
+           JOIN properties p ON p.id = e.property_id
+           JOIN addresses a ON a.id = e.address_id
+           JOIN districts d ON d.id = a.district_id
+           JOIN streets s ON d.id = s.district_id", id, "e");
 }
+
+async Task<IResult> ReadSeekersAsync(int page, int pageSize, int? id = null)
+{
+    return await ReadPageAsync(page, pageSize,
+        $@"SELECT s.id, s.first_name, s.last_name, s.patronymic,
+                  stat.status, p.position, s.education
+                  s.phone, s.birthday, s.registration_city,
+                  s.recommended, s.pol, d.district,
+                  st.street, a.building_number, st.postal_code
+           FROM seekers s
+           JOIN statuses stat ON stat.id = s.status_id
+           JOIN positions p ON p.id = s.speciality_id
+           JOIN addresses a ON a.id = s.address_id
+           JOIN districts d ON d.id = a.district_id
+           JOIN streets st ON d.id = st.district_id", id, "s");
+}
+
+async Task<IResult> ReadAddressesAsync(int page, int pageSize, int? id = null)
+{
+    return await ReadPageAsync(page, pageSize,
+        $@"SELECT a.id, d.district, s.street, a.building_number, s.postal_code
+           FROM addresses a
+           JOIN districts d ON d.id = a.district_id
+           JOIN streets s ON d.id = s.district_id", id, "a");
+}
+
+async Task<IResult> ReadVacanciesAsync(int page, int pageSize, int? id = null)
+{
+    return await ReadPageAsync(page, pageSize,
+        $@"SELECT v.id, v.employer_id, p.position, v.employer_day,
+                  v.salary_new, v.chart_new, v.vacancy_end
+           FROM vacancies v
+           JOIN positions p ON p.id = v.position_id", id, "v");
+}
+
+async Task<IResult> ReadApplicationsAsync(int page, int pageSize, int? id = null)
+{
+    return await ReadPageAsync(page, pageSize,
+        $@"SELECT a.id, a.seeker_id, p.position, et.type,
+                  a.seeker_day, a.information, a.photo, a.experience
+           FROM applications a
+           JOIN positions p ON p.id = a.position_id
+           JOIN employment_types et ON et.id = a.employment_type_id", id, "a");
+}
+
+async Task<IResult> ReadStreetsAsync(int page, int pageSize, int? id = null)
+{
+    return await ReadPageAsync(page, pageSize,
+        $@"SELECT s.id, d.district, s.street, s.postal_code
+           FROM streets s
+           JOIN districts d ON d.id = s.district_id", id, "s");
+}
+
+async Task<IResult> ReadAsync(int page, int pageSize, string target, int? id = null)
+{
+    return await ReadPageAsync(page, pageSize, $@"SELECT * FROM {target}", id);
+}
+
+async Task<IResult> ReadPageAsync(int page, int pageSize, string command, int? id = null, string? tableAlias = null)
+{
+    tableAlias = tableAlias is null ? "" : tableAlias + ".";
+    return Results.Ok(await postgres.ExecuteAsync(
+        $@"{command}
+           {(id is null ? "" : $"WHERE {tableAlias}id = {id}")}
+           OFFSET {page * pageSize}
+           LIMIT {pageSize}; "
+    ));
+}
+
+delegate Task<IResult> ReadAsyncFunc(int page, int pageSize, int? id = null);
