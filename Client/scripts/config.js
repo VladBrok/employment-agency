5c83c8864468367e9af7d3fee233c6ab74991bc8
Vlad Brok <vlad.brok99@gmail.com>
2022-07-04 11:06:11 +0300

    Add favicon

--- Client/scripts/config.js
+++ Client/scripts/config.js
@@ -1,4 +1,4 @@
-const isDev = true; // It changes via the publish.sh script
+const isDev = false; // It changes via the publish.sh script
 const apiUrl = isDev
   ? "https://localhost:7288/api"
   : "https://bsite.net/employmentAgencyApi/api";
