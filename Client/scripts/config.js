d089f7cbe37b2be09d1e4f6bccfc3a6ee64974b1
Vlad Brok <vlad.brok99@gmail.com>
2022-08-28 13:20:37 +0300

    Change order of api urls in config

--- Client/scripts/config.js
+++ Client/scripts/config.js
@@ -2,7 +2,7 @@ const isDev = false; // It changes via the publish.sh script
 const apiUrls = isDev
   ? ["https://localhost:7288/api"]
   : [
-      "http://www.employmentagencyapi.somee.com/api",
-      "https://bsite.net/employmentAgencyApi/api",
+    "https://bsite.net/employmentAgencyApi/api",
+    "http://www.employmentagencyapi.somee.com/api",
     ];
 export { apiUrls };
