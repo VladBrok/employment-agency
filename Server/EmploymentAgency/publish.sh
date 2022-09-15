336bcbcf190c13ca80f9c50a7defc9f7f4e7bd18
Vlad Brok <vlad.brok99@gmail.com>
2022-09-15 22:30:12 +0300

    Remove Server.zip before creating a new one

--- Server/EmploymentAgency/publish.sh
+++ Server/EmploymentAgency/publish.sh
@@ -3,4 +3,5 @@
 dotnet publish -c Release -o bin/build/
 cp -r media/ bin/build/
 cp -r sql/ bin/build/
+rm -rf Server.zip
 powershell Compress-Archive -Path 'bin\build\*' -DestinationPath 'Server.zip' -Force
